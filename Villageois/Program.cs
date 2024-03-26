ee46202b0b4d59326ee15d85c4aa53c2b9d4acc7
belkhir_y <belkhir_y@D202-022024-03>
2024-03-26 10:42:49 +0100

    Ajoutez des fichiers projet.

---
+++ Villageois/Program.cs
@@ -0,0 +1,19 @@
+﻿using libVillageois;
+
+Villageois a, b;
+string rep;
+a = new Villageois("Panoramix", 16, true);
+b = new Villageois("Abraracoourcix");
+Console.WriteLine(b.getPoids());
+b.setPoids(26);
+Console.WriteLine(a.ToString());
+Console.WriteLine(a.getNom());
+Console.WriteLine(a.getPoids());
+Console.WriteLine(a.poidsSouleve()+"Kg");
+Console.WriteLine(b.getPoids());
+
+a.setDose(3);
+Console.WriteLine( $"{a.getNom()} prend {a.} doses");
+Console.WriteLine("Apres les doses "+a.plusFort(b));
+Console.WriteLine($"{a.getNom()} soulève {a.poidSouleve()} poids");
+Console.WriteLine($"{b.getNom()} soulève {b.poidsSouleve()} poids");
---
+++ libVillageois/Villageois.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libVillageois
+{
+    public class Villageois
+    {
+        private string nom;
+
+        private int poids;
+
+        private bool malade;
+
+        private Potion maPotion;
+
+
+        public Villageois(string nom)
+        {
+            this.nom = nom;
+            this.malade = false;
+        }
+
+        public Villageois(string nom, int poids, bool malade)
+        {
+            this.nom = nom;
+            this.poids = poids;
+            this.malade = malade;
+
+        }
+        public Villageois(string nom, int poids, bool malade, int potion)
+        {
+            this.nom = nom;
+            this.poids = poids;
+            this.malade = malade;
+            this.maPotion = new Potion(potion);
+
+        }
+
+        public string getNom() { return this.nom; }
+
+        public int getPoids() { return this.poids; }
+
+        public bool getMalade() { return this.malade; }
+
+        public void setPoids(int p) { this.poids = p; }
+
+        public override string ToString()
+        {
+            string mal = "Malade";
+            if (!this.malade) mal = "pas malade";
+            return $"Le villageois se nomme {nom} peut soulever {poids} et est {mal} ";
+        }
+
+        public int poidsSouleve()
+
+        {
+            return poids / 5;
+        }
+
+        public void setdoses(int n)
+
+        {
+            maPotion.ajouterDose(n);
+
+        }
+
+        public Potion 
+            getPotion() 
+        { 
+           return maPotion; 
+        }
+        
+      
+        
+       
+
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+}
